4433185a0081057f569223c870b66d4a04ce362f
Diego HS <diego.hidalgo@hotmail.es>
2023-12-10 10:16:09 +0100

    📝 added some data hauses.

--- src/scrapping/idealista_scraping_hauses.py
+++ src/scrapping/idealista_scraping_hauses.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 import json
 import random
@@ -10,7 +11,7 @@ os.system("clear")
 
 url_base = "https://www.idealista.com/inmueble"
 headers = {
-    # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.106 Safari/537.36',
+    # "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.200 Safari/537.36",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
     "set-cookie": "_pxCaptcha=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;",
 }
@@ -29,8 +30,10 @@ for barrio in list(hauses_ids.keys()):
 
     ids_list = hauses_ids[barrio]
 
-    hause_info = {"barrio": barrio}
     for hause_id in ids_list:
+        hause_info = {"barrio": barrio}
+        secs = 2.0 + random.random()
+
         if hause_id in list(all_hauses_info.keys()):
             print(f"\tHause ID {hause_id} already in our database, continue.")
             continue
@@ -41,7 +44,17 @@ for barrio in list(hauses_ids.keys()):
         hause_info.update({"hause_id": hause_id, "url": url})
 
         r = requests.get(url, headers=headers)
+        # check for the same url (sometimes changes)
+        returned_id = re.findall(r"\d+", r.url)[0]
+        if hause_id != returned_id:
+            print(
+                f"\tHause ID: {hause_id} is not the same as the returned ID: {returned_id}"
+            )
+            time.sleep(secs)
+            continue
+
         html_page = BeautifulSoup(r.text, "html.parser")
+        print(html_page)
 
         # precio
         html_price = html_page.select(".detail-content-wrapper .info-data-price")
@@ -52,8 +65,11 @@ for barrio in list(hauses_ids.keys()):
         html_text = html_page.select(
             ".detail-content-wrapper .commentsContainer .comment p"
         )
-        text = html_text[0].text
-        hause_info.update({"text": text})
+        if len(html_text) > 0:
+            text = html_text[0].text
+            hause_info.update({"text": text})
+        else:
+            hause_info.update({"text": "NULL"})
 
         # details
         details = []
@@ -80,7 +96,6 @@ for barrio in list(hauses_ids.keys()):
         all_hauses_info.update({hause_id: hause_info})
 
         # waiting time to avoid being banned
-        secs = 2.0 + random.random()
         time.sleep(secs)
 
         # save in every step to avoid on starting from the beginning
