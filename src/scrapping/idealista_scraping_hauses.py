8515d3154c60e75677c5110440afc3b2a571fae1
Diego HS <diego.hidalgo@hotmail.es>
2023-12-10 14:00:58 +0100

    🚀 added more User-Agents.

--- src/scrapping/idealista_scraping_hauses.py
+++ src/scrapping/idealista_scraping_hauses.py
@@ -10,10 +10,42 @@ from bs4 import BeautifulSoup
 os.system("clear")
 
 url_base = "https://www.idealista.com/inmueble"
+
+
+# https://www.useragents.me/
+ua = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1.2 Safari/605.1.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Herring/90.1.9310.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.4",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+]
 headers = {
-    # "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.200 Safari/537.36",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
-    "set-cookie": "_pxCaptcha=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;",
+    # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
+    # "Cache-Control": "no-cache",
+    # "Connection": "keep-alive",
+    # "Pragma": "no-cache",
+    # "Sec-Ch-Ua": '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    # "Sec-Fetch-Dest": "document",
+    # "Sec-Fetch-Mode": "navigate",
+    # "Sec-Fetch-Site": "none",
+    # "Sec-Fetch-User": "?1",
+    # "Upgrade-Insecure-Requests": "1",
+    "Set-Cookie": "_pxCaptcha=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;",
+    "User-Agent": random.choice(ua),
 }
 
 with open("data_hause/hause_ids.json", "r") as fp:
@@ -32,7 +64,7 @@ for barrio in list(hauses_ids.keys()):
 
     for hause_id in ids_list:
         hause_info = {"barrio": barrio}
-        secs = 2.0 + random.random()
+        secs = 2.0 + random.random() * 5.0
 
         if hause_id in list(all_hauses_info.keys()):
             print(f"\tHause ID {hause_id} already in our database, continue.")
@@ -43,7 +75,13 @@ for barrio in list(hauses_ids.keys()):
         url = os.path.join(url_base, hause_id)
         hause_info.update({"hause_id": hause_id, "url": url})
 
-        r = requests.get(url, headers=headers)
+        with requests.Session() as s:
+            r = s.get(url, headers=headers)
+        if "User-Agent" not in list(r.headers.keys()):
+            print(f"\tERROR: No 'User-Agent' in connection.")
+            time.sleep(secs)
+            continue
+
         # check for the same url (sometimes changes)
         returned_id = re.findall(r"\d+", r.url)[0]
         if hause_id != returned_id:
@@ -54,7 +92,6 @@ for barrio in list(hauses_ids.keys()):
             continue
 
         html_page = BeautifulSoup(r.text, "html.parser")
-        print(html_page)
 
         # precio
         html_price = html_page.select(".detail-content-wrapper .info-data-price")
