6e97119d71110f34d0602e6393e55ae714f7916b
Diego HS <diego.hidalgo@hotmail.es>
2023-12-15 16:41:49 +0100

    ✨ added Idealista API connection.

--- src/api/idealista_api.py
+++ src/api/idealista_api.py
@@ -1,67 +1,99 @@
 import os
 import time
 import json
+import random
 import requests
 from requests.auth import HTTPBasicAuth
+from typing import Dict, Any
 
+import pandas as pd
 from dotenv import load_dotenv
 
 os.system("clear")
 # set your path enviromental file
-load_dotenv("/Users/diegohs/Documents/MASTER/visual_casas/src/.env")
+load_dotenv("/home/oxcom/visual_casas/src/.env")
 
 key = os.getenv("API_KEY")
 secret = os.getenv("API_SECRET")
 
+
 # * First: get the access token from idealista API
-oauth_url = "https://api.idealista.com/oauth/token"
-payload = {"grant_type": "client_credentials"}
-r = requests.post(oauth_url, auth=HTTPBasicAuth(key, secret), data=payload)
-token = json.loads(r.text)["access_token"]
+def get_token() -> str:
+    oauth_url = "https://api.idealista.com/oauth/token"
+    payload = {"grant_type": "client_credentials"}
+    r = requests.post(oauth_url, auth=HTTPBasicAuth(key, secret), data=payload)
+    token = json.loads(r.text)["access_token"]
+
+    return token
+
+
+def get_url(params: Dict[str, Any]) -> str:
+    base_url = params["base_url"]
+    country = params["country"]
+    operation = params["operation"]
+    max_items = params["max_items"]
+    center = params["center"]
+    distance = params["distance"]
+    property_type = params["property_type"]
+    language = params["language"]
+    pagination = params["pagination"]
+
+    url = f"{base_url}/{country}/search?operation={operation}&maxItems={max_items}&center={center}&distance={distance}"
+    url += f"&propertyType={property_type}&language={language}&numPage={pagination}"
+    # url += f"&order={order}&sort={sort}&maxPrice={maxprice}"
+
+    return url
+
 
 # basic params
-base_url = "https://api.idealista.com/3.5/"  # Base search url
-country = "es"  # Search country (es, it, pt)
-language = "es"  # Search language (es, it, pt, en, ca)
-max_items = "50"  # Max items per call, the maximum set by Idealista is 50
-operation = "sale"  # Kind of operation (sale, rent)
-property_type = (
-    "homes"  # Type of property (homes, offices, premises, garages, bedrooms)
-)
-order = "priceDown"  # Order of the listings, consult documentation for all the available orders
-center = "38.34517,-0.48149"  # Coordinates of the search center
-distance = "60000"  # Max distance from the center
-sort = "desc"  # How to sort the found items
-# bankOffer = "false"  # If the owner is a bank
-# maxprice = "750"  # Max price of the listings
-
-url = (
-    base_url
-    + country
-    + "/search?operation="
-    + operation
-    + "&maxItems="
-    + max_items
-    + "&order="
-    + order
-    + "&center="
-    + center
-    + "&distance="
-    + distance
-    + "&propertyType="
-    + property_type
-    + "&sort="
-    + sort
-    + "&numPage=%s"
-    # + "&maxPrice="
-    # + maxprice
-    + "&language="
-    + language
-)
+params = {
+    "base_url": "https://api.idealista.com/3.5",  # Base search url
+    "country": "es",  # Search country (es, it, pt)
+    "language": "es",  # Search language (es, it, pt, en, ca)
+    "max_items": "50",  # Max items per call, the maximum set by Idealista is 50
+    "operation": "sale",  # Kind of operation (sale, rent)
+    "property_type": "homes",  # Type of property (homes, offices, premises, garages, bedrooms)
+    "center": "38.34517,-0.48149",  # Coordinates of the search center (Alicante)
+    "distance": "25000",  # Max distance from the center [m]
+    # "sort": "desc",  # How to sort the found items
+    # "order": "priceDown",  # Order of the listings, consult documentation for all the available orders
+    # "maxprice": "750",  # Max price of the listings
+    # "bankOffer": "false",  # If the owner is a bank
+}
 
 # connexion
-headers = {"Authorization": "Bearer " + token}
-content = requests.post(url, headers=headers)
-result = json.loads(content.text)
+i = 1
+while True:
+    # secs = 1.0
 
-print(result)
+    params.update({"pagination": i})
+    url = get_url(params=params)
+
+    if i == 1:
+        token = get_token()
+        headers = {"Authorization": f"Bearer {token}"}
+
+    content = requests.post(url, headers=headers)
+    print(content)
+    result = json.loads(content.text)
+
+    # print(json.dumps(result, indent=4))
+    data = result["elementList"]
+    if i == 1:
+        df = pd.DataFrame.from_dict(data)
+    else:
+        df = pd.concat([df, pd.DataFrame.from_dict(data)])
+
+    total_pages = result["totalPages"]
+    print(f"Iteration {i} of {total_pages}")
+    if i < total_pages:
+        i += 1
+        # secs += random.random() * 3.0
+        # print(f"Waiting {round(secs, 2)} seconds for the next iteration.")
+        # time.sleep(secs)
+    else:
+        break
+
+    # break
+
+df.to_csv("data/data_hauses.csv", index=False, sep="\t")
